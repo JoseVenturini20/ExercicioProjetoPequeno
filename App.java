12da470d3d05b448fbab7f91043391b0bb670e74
AlexElias <alex.elias@finmatch.com.br>
2021-03-21 16:28:21 -0300

    Adicionando App para testes

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String args[]){
+        CatalogoVeiculos c = new CatalogoVeiculos();
+        c.consultarPorTipo("VeiculoPassageiro");
+        //c.consultarPorTipo("VeiculoPasseio");
+        //c.consultarPorTipo("VeiculoUtilitario");
+    }
+}
