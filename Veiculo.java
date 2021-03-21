c425dec0dcee16198e51899e2cea856a87817cea
José <jose@Joses-MacBook-Pro.local>
2021-03-21 13:35:44 -0300

    Interface Veiculo

---
+++ Veiculo.java
@@ -0,0 +1,8 @@
+public interface Veiculo {
+    public String placa = "";
+    public String marca = "";
+    public String modelo = "";
+    public Integer ano = 0;
+    public Double valor = 0.0;
+
+}
