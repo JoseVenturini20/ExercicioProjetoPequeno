880946a37d00fc4e27b2c9a0c43385aac66b65b6
AlexElias <alex.elias@finmatch.com.br>
2021-03-21 18:11:09 -0300

    Adicionando get de informações

--- Veiculo.java
+++ Veiculo.java
@@ -1,8 +1,8 @@
 public interface Veiculo {
-    public String placa = "";
-    public String marca = "";
+    public String getPlaca();
+    public String getMarca();
     public String modelo = "";
-    public Integer ano = 0;
+    public Integer getAno();
     public Double valor = 0.0;
 
 }
